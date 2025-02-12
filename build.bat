76433112e1d9efd9a15f8dbc010f0429f57fa692
Douglas S. Elias <douglassantoselias@gmail.com>
2025-02-12 16:42:53 -0300

    Add build.bat

---
+++ build.bat
@@ -0,0 +1,19 @@
+@echo off
+
+cl
+
+if %ERRORLEVEL% neq 0 (
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+)
+
+cls
+
+rmdir /S /Q .\build
+mkdir build
+pushd .\build
+
+cl /nologo /diagnostics:caret /WX /W4 /wd4189 /wd4996 /wd4100 /wd4244 ..\main.c
+
+main.exe amd
+
+popd
