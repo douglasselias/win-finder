a58c97ce48337793f6b576132b8525e7ef4e8e3b
Douglas S. Elias <douglassantoselias@gmail.com>
2025-03-22 00:40:58 -0300

    Remove unused code

--- main.c
+++ main.c
@@ -1,66 +1,56 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdint.h>
-#include <ctype.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
 #include <time.h>
-#include <stdlib.h>
 #include <intrin.h>
-#include <assert.h>
 
-typedef int64_t  s64;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef double   f64;
-
-void list_files_from_dir(const char* path);
+typedef int64_t s64;
+typedef int32_t s32;
+typedef double  f64;
 
 typedef bool string_match_proc_t(const char* string, const char* pattern);
 
-bool has_substring(const char* haystack, const char* needle) {
-  u64 haystack_length = strlen(haystack);
-  u64 needle_length   = strlen(needle);
+bool has_substring(const char* haystack, const char* needle);
+bool simple_fuzzy_match(const char* string, const char* pattern);
 
-  if(needle_length == 0) return true;
-  if(haystack_length < needle_length) return false;
+DWORD thread_proc(void* args);
+void add_work(char dir[MAX_PATH]);
+void list_files_from_dir(const char* path);
 
-  u64 haystack_index = 0;
-  u64 needle_index   = 0;
+char query[MAX_PATH];
+string_match_proc_t *string_match_proc = has_substring;
 
-  while(haystack_index < haystack_length) {
-    if(tolower(haystack[haystack_index]) == tolower(needle[needle_index])) {
-      needle_index++;
+int main(int argc, char* argv[]) {
+  strcpy(query, argv[2]);
 
-      if(needle_index < needle_length) haystack_index++;
-      else return true;
-    } else {
-      needle_index = 0;
-      haystack_index++;
-    }
+  if(argv[3] != NULL && strcmp(argv[3], "fuzzy") == 0) {
+    string_match_proc = simple_fuzzy_match;
   }
 
-  return false;
-}
-
-bool simple_fuzzy_match(const char* string, const char* pattern) {
-  while(*string != '\0' && *pattern != '\0')  {
-    if(*string + 32 == *pattern + 32)
-      pattern++;
-    string++;
-  }
-
-  return *pattern == '\0' ? true : false;
-}
-
-u32 count_threads() {
   SYSTEM_INFO sysinfo;
   GetSystemInfo(&sysinfo);
-  return sysinfo.dwNumberOfProcessors;
+  s32 total_threads = sysinfo.dwNumberOfProcessors;
+  HANDLE* threads = calloc(sizeof(HANDLE), total_threads);
+
+  puts("Searching...\n");
+
+  clock_t start = clock();
+
+  list_files_from_dir(argv[1]);
+
+  for(s64 i = 0; i < total_threads; i++) {
+    threads[i] = CreateThread(NULL, 0, thread_proc, NULL, 0, NULL);
+  }
+  WaitForMultipleObjects(total_threads, threads, true, INFINITE);
+
+  clock_t end = clock();
+
+  f64 cpu_time_used = (end - start) / CLOCKS_PER_SEC;
+  printf("\nDone in %.2f seconds\n", cpu_time_used);
 }
 
-/// @note: This need to be a big number, since to complete each task, it needs to iterate all files in the specified directory. However, while doing this, it adds the directories that were found while iterating the files, thus creating way more tasks to do than its possible to finish.
 #define MAX_NUMBER_OF_TASKS 1000000
 char work_to_do[MAX_NUMBER_OF_TASKS][MAX_PATH] = {0};
 volatile s64 write_index = 0;
@@ -113,62 +103,61 @@ DWORD thread_proc(void* args) {
   return 0;
 }
 
-char query[70] = {};
-string_match_proc_t *string_match_proc = has_substring;
-
-void list_files_from_dir(const char* path) {
+void list_files_from_dir(const char* current_dir) {
   char search_path[MAX_PATH] = {0};
-  strcat(search_path, path);
-  strcat(search_path, "\\*");
+  sprintf(search_path, "%s\\*", current_dir);
 
   WIN32_FIND_DATA find_file_data;
   HANDLE find_file_handle = FindFirstFile(search_path, &find_file_data);
-  if(find_file_handle == INVALID_HANDLE_VALUE) return;
 
   do {
     bool is_dir = find_file_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY;
     char* filename = find_file_data.cFileName;
     if(is_dir) {
       if(filename[0] != '.') {
-        char dir_name[MAX_PATH] = {0};
-        strcat(dir_name, path);
-        strcat(dir_name, "\\");
-        strcat(dir_name, filename);
-        add_work(dir_name);
+        char dir[MAX_PATH] = {0};
+        sprintf(dir, "%s\\%s", current_dir, filename);
+        add_work(dir);
       }
     } else {
       if((*string_match_proc)(filename, query)) {
-        printf("%s\\%.20s\n", path, filename);
+        printf("%s\\%s\n", current_dir, filename);
       }
     }
   } while(FindNextFile(find_file_handle, &find_file_data) != 0);
 }
 
-int main(int argc, char* argv[]) {
-  char current_directory[MAX_PATH] = {0};
-  strcpy(current_directory, argv[1]);
-  strcpy(query, argv[2]);
+bool has_substring(const char* haystack, const char* needle) {
+  s64 haystack_length = strlen(haystack);
+  s64 needle_length   = strlen(needle);
 
-  if(argv[3] != NULL && strcmp(argv[3], "fuzzy") == 0) {
-    string_match_proc = simple_fuzzy_match;
+  if(needle_length == 0) return true;
+  if(needle_length > haystack_length) return false;
+
+  s64 haystack_index = 0;
+  s64 needle_index   = 0;
+
+  while(haystack_index < haystack_length) {
+    if(tolower(haystack[haystack_index]) == tolower(needle[needle_index])) {
+      needle_index++;
+
+      if(needle_index < needle_length) haystack_index++;
+      else return true;
+    } else {
+      needle_index = 0;
+      haystack_index++;
+    }
   }
 
-  u32 total_threads = count_threads() - 1;
-  HANDLE* threads = calloc(sizeof(HANDLE), total_threads);
+  return false;
+}
 
-  puts("Searching...\n");
-
-  clock_t start = clock();
-
-  list_files_from_dir(current_directory);
-
-  for(u64 i = 0; i < total_threads; i++) {
-    threads[i] = CreateThread(NULL, 0, thread_proc, NULL, 0, NULL);
+bool simple_fuzzy_match(const char* string, const char* pattern) {
+  while(*string != '\0' && *pattern != '\0')  {
+    if(*string + 32 == *pattern + 32)
+      pattern++;
+    string++;
   }
-  WaitForMultipleObjects(total_threads, threads, true, INFINITE);
 
-  clock_t end = clock();
-
-  f64 cpu_time_used = (end - start) / CLOCKS_PER_SEC;
-  printf("\nDone in %.2f seconds\n", cpu_time_used);
+  return *pattern == '\0' ? true : false;
 }
